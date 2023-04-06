1dcdaaf9ce82a25de664b53cd402a854b45d2a9c
Andrew Davison <andrew.davison@centre.edu>
2023-04-06 11:43:44 -0400

    Added Stuff for P4

---
+++ AlphadokuSolver/Alphadoku.cpp
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+
+int main() {
+    system(".\\slime.exe test.cnf");
+}
--- SATSolvers/HillClimbing.cpp
+++
@@ -1,4 +0,0 @@
-//Written by Andrew Davison for Project A3 for CSC 372: Artificial Intelligence with Dr. Thomas Allen
-//Implements the simple hill climbing method for solving SAT
-//Implements the function simpleHillClimbing in SAT.hpp
-
